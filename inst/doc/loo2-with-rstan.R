2dd22813e5310433eaf768488779d760ae58e54c
Jonah Gabry <jsg2201@columbia.edu>
2018-04-11 11:34:58 +0000

    version 2.0.0

--- inst/doc/loo2-with-rstan.R
+++ inst/doc/loo2-with-rstan.R
@@ -1,3 +1,6 @@
+params <-
+structure(list(EVAL = TRUE), .Names = "EVAL")
+
 ## ---- eval=FALSE---------------------------------------------------------
 #  library("rstan")
 #  
@@ -16,22 +19,29 @@
 #  library("loo")
 #  
 #  # Extract pointwise log-likelihood and compute LOO
-#  log_lik_1 <- extract_log_lik(fit_1)
-#  loo_1 <- loo(log_lik_1)
+#  log_lik_1 <- extract_log_lik(fit_1, merge_chains = FALSE)
+#  
+#  # as of loo v2.0.0 we can optionally provide relative effective sample sizes
+#  # when calling loo, which allows for better estimates of the PSIS effective
+#  # sample sizes and Monte Carlo error
+#  r_eff <- relative_eff(exp(log_lik_1))
+#  
+#  loo_1 <- loo(log_lik_1, r_eff = r_eff, cores = 2)
 #  print(loo_1)
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  standata$X[, "arsenic"] <- log(standata$X[, "arsenic"])
 #  fit_2 <- stan(fit = fit_1, data = standata)
 #  
-#  log_lik_2 <- extract_log_lik(fit_2)
-#  loo_2 <- loo(log_lik_2)
+#  log_lik_2 <- extract_log_lik(fit_2, merge_chains = FALSE)
+#  r_eff_2 <- relative_eff(exp(log_lik_2))
+#  loo_2 <- loo(log_lik_2, r_eff = r_eff_2, cores = 2)
 #  print(loo_2)
 
 ## ---- eval=FALSE---------------------------------------------------------
 #  # Compare
-#  diff <- compare(loo_1, loo_2)
+#  comp <- compare(loo_1, loo_2)
 
 ## ---- eval=FALSE---------------------------------------------------------
-#  print(diff)
+#  print(comp)
 
